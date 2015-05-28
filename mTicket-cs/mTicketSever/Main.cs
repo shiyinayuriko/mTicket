721f2dd7474421909df8948d173132fd4c7b75d3
shiyinayuriko <whitecometliliwill@gmail.com>
2015-05-28 23:01:52 +0800

    Form1重命名

--- mTicket-cs/mTicketSever/Main.cs
+++ mTicket-cs/mTicketSever/Main.cs
@@ -15,9 +15,9 @@ using mTickLibs.Tools;
 
 namespace mTicket
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
             CheckForIllegalCrossThreadCalls = false;
